b98b5f7a9fa88dbbb5c3ae49fa81de2449d7d216
Vansh Thapar <vanshthapar.professional@gmail.com>
2025-08-12 09:57:28 +0530

    implemented dynamic prompting by creating an array and saving the previous prompts in it

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
 GEMINI_API_KEY = "AIzaSyBqnY1tQGYdENf4Zq5vbYsNMXXU56k0nOs"
 
+// for dynamic prompting i am initializing an empty array in this i will be pushing all the queries that the user asks from our llm
+let conversationHistory = [];
+
+async function dynamicPrompting(){
+    const inputEl = document.getElementById('input') 
+    const input = inputEl.value;
+    if(input == ""){
+        alert("Your have entered nothing");
+        return;
+    }
+    let systemInstructions = `
+        You are HealthGot, an AI medical report explainer.
+        Analyze the provided medical document, explain medical terms in simple language, and highlight any important findings.
+        Avoid giving medical advice — only explain.
+        Here is the conversation so far:
+    `;
+    conversationHistory.push({role: "user" , text: input});
+    conversationHistory.forEach((message,index)=>{
+        systemInstructions+=`\n${message.role === 'user' ? "User" :"AI"}: ${message.text}`
+    })
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`
+        ,{
+            method:"POST",
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify({
+                contents:[
+                    {
+                        parts:[{text: systemInstructions}]
+                    }
+                ]
+            })
+        }
+    );
+    const data = await response.json();
+    const aiResponse = data.candidates?.[0]?.content?.parts?.[0].text || "No response from AI.";
+    console.log(data);
+
+    conversationHistory.push({role:"ai" , text: aiResponse})
+
+    const outputEl = document.getElementById('output');
+    outputEl.innerHTML = `<h1 class="text-2xl p-5">${aiResponse}</h1>`
+}
 async function zeroShotPrompting(){
     const inputEl = document.getElementById('input') 
     const input = inputEl.value;
